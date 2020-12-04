a888cbcac40167990ce5b3aa9173bfe08e66a524
Sebastián Maciel <sebastianmaciel@outlook.com>
2020-12-04 15:55:42 -0300

    Reacomodadas las funciones en utils.js

--- js/index.js
+++ js/index.js
@@ -1,51 +1,21 @@
 // Cripto a buscar
 const nombreCripto = "bitcoin";
 
-// Lista de monedas
+// Lista de monedas - Por el momento nos interesan pesos y dólares
 const monedas = {
   dolares: "usd",
   pesos: "ars",
 };
 
-// Esta función simplifica la búsqueda de criptos y conversión a divisas.
-// Apunta a que mañana pueda ser utilizada para buscar otros datos
-const buscarPrecios = async (nombreCripto, monedas) => {
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${nombreCripto}&vs_currencies=${monedas.dolares},${monedas.pesos}`;
-
-  try {
-    fetch(url)
-      .then((datosEnBruto) => datosEnBruto.json())
-      .then((datosJSON) => {
-        // Desestructuramos todo el json que vino de la api
-        let precio = ({} = datosJSON);
-
-        const bitcoinDolares = precio[nombreCripto][monedas.dolares];
-        const bitcoinPesos = precio[nombreCripto][monedas.pesos];
-
-        const usd = accounting.formatMoney(bitcoinDolares, "$", 0, ".", ",");
-        const ars = accounting.formatMoney(bitcoinPesos, "$", 0, ".", ",");
-
-        console.log(usd);
-        console.log(ars);
-        mostrarPrecios(ars, usd);
-      });
-  } catch (error) {
-    console.log("Algo muuuuy malo sucedió...");
-  }
-};
-
-const mostrarPrecios = (ars, usd) => {
-  document.getElementById("ars").textContent = ars;
-  document.getElementById("usd").textContent = usd;
-};
-
-// Cuando hacemos click en el botón Actualizar
-document.getElementById("actualizar").addEventListener("click", () => {
-  buscarPrecios(nombreCripto, monedas);
-  actualizarHora();
-});
-
 // Cuando inicia la página, actualizamos la hora actual
 actualizarHora();
 // Mostramos por primera vez los precios
 buscarPrecios(nombreCripto, monedas);
+
+// Cuando hacemos click en el botón Actualizar
+document.getElementById("actualizar").addEventListener("click", () => {
+  // Volvemos a ejecutar la búsqueda de precios
+  buscarPrecios(nombreCripto, monedas);
+  // Actualizamos la hora de nuevo
+  actualizarHora();
+});
--- js/utils.js
+++ js/utils.js
@@ -8,3 +8,33 @@ const actualizarHora = () => {
     ? (document.getElementById("hora").textContent = `${horas}:0${minutos}`)
     : (document.getElementById("hora").textContent = `${horas}:${minutos}`);
 };
+
+// Esta función simplifica la búsqueda de criptos y conversión a divisas.
+// Apunta a que mañana pueda ser utilizada para buscar otros datos
+const buscarPrecios = async (nombreCripto, monedas) => {
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${nombreCripto}&vs_currencies=${monedas.dolares},${monedas.pesos}`;
+
+  try {
+    fetch(url)
+      .then((datosEnBruto) => datosEnBruto.json())
+      .then((datosJSON) => {
+        // Desestructuramos todo el json que vino de la api
+        let precio = ({} = datosJSON);
+
+        const bitcoinDolares = precio[nombreCripto][monedas.dolares];
+        const bitcoinPesos = precio[nombreCripto][monedas.pesos];
+
+        const usd = accounting.formatMoney(bitcoinDolares, "$", 0, ".", ",");
+        const ars = accounting.formatMoney(bitcoinPesos, "$", 0, ".", ",");
+
+        mostrarPrecios(ars, usd);
+      });
+  } catch (error) {
+    console.log("Algo muuuuy malo sucedió...");
+  }
+};
+
+const mostrarPrecios = (ars, usd) => {
+  document.getElementById("ars").textContent = ars;
+  document.getElementById("usd").textContent = usd;
+};
