fb5a00e539d4c79038f79499ac9f0145f2541b7c
Sebastián <sebastianmaciel@outlook.com>
2020-12-05 16:42:03 -0300

    Replanteamiento de funciones... Agregados BTC ETH y DAI

--- js/index.js
+++ js/index.js
@@ -1,27 +1,33 @@
+// =======================================================================================
 // Setup inicial!
-// ==============
-// Queremos que la web cargue los datos por lo menos una vez.
-// Después, con el botón actualizar hacemos lo mismo cada vez que queramos.
-
 // Seteamos el nomobre de la criptomoneda a buscar:
-const nombreCripto = "bitcoin";
+// =======================================================================================
+const botonesPrecio = document.querySelectorAll(".buscarPrecio");
 
+botonesPrecio.forEach(function (boton) {
+  boton.addEventListener("click", (e) => {
+    const cripto = e.target.dataset.id; // en el botón está el dato que nos interesa
+    document.getElementById("nombreCripto").textContent = cripto; // Nombre del cripto
+    // Falta cambiar loguito...
+    // document.getElementById("logoCripto").textContent = cripto;
+    // Actualizamos...
+    actualizarPrecios(cripto, divisas);
+  });
+});
+
+// const cripto = "bitcoin";
+
+// =======================================================================================
 // Hacemos una lista de monedas - Por el momento nos interesan pesos y dólares
-// Estos valores realmente se envían como argumentos a una función
-// que se encarga de separar estos valores para enviarlos a la api de consulta
+// =======================================================================================
 const divisas = {
   dolares: "usd",
   pesos: "ars",
 };
 
-// Esta función pone en pantalla la hora actual y los precios
-// Cada vez que cargamos la página por primera vez o cuando refrezcamos el sitio
-actualizarHoraYPrecios();
-
+// =======================================================================================
 // Cuando hacemos click en el botón Actualizar
-document.getElementById("actualizar").addEventListener("click", () => {
-  // Volvemos a ejecutar la búsqueda de precios
-  actualizarPrecios(nombreCripto, divisas);
-  // Actualizamos la hora de nuevo
-  actualizarHora();
-});
+// =======================================================================================
+// document.getElementById("actualizar").addEventListener("click", () => {
+//   actualizarPrecios(cripto, divisas); // Volvemos a ejecutar la búsqueda de precios
+// });
